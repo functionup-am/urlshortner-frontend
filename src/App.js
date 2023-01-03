ac3084bb20e3a1be8847dac102a003158a4b8245
functionup-am <akshaymakwana350@gmail.com>
2023-01-03 19:12:46 +0530

    url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+const UrlHandler = () =>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [longUrl, setLongUrl] = useState('')
+  const [shortUrl,setShortUrl] = useState('')   
+  const postData = (data) =>{
+       
+         fetch('http://localhost:3001/url/shorten',{
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+          longUrl:data
+        })
+       }) 
+       .then(response => response.json())
+       .then((data) => {
+        setShortUrl(data.data.shortUrl)   
+      })
+      setLongUrl('');                                                            
+           
+  }              
 
-export default App;
+   return <div className='url'>
+        <h4>Short your LongUrl</h4>
+        <h5>URL: <input type="text" size={50} value={longUrl} onChange={(e)=> setLongUrl(e.target.value)}/> </h5>
+        <button onClick={()=>postData(longUrl)}> Submit </button>
+        <h3>{shortUrl}</h3>
+      </div>
+}  
+
+export default UrlHandler;       
